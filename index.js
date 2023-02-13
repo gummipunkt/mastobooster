a930a0aa6ef3e59dfca0bc54c45cdf9136f0c8bd
gummipunkt <patti2012@gmail.com>
2023-02-13 13:02:10 +0100

    Update index.js
    minor changes and improvements

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/* 
-import modules 
-*/
+/* import modules */
 // Mastodon
 import { login } from 'masto';
 // ENV
@@ -20,20 +18,70 @@ const masto = await login({
 	accessToken: process.env.TOKEN,
 });
 
-// Hashtag generation
+// Import Hashtag
 const hashtag = process.env.HASHTAG;
 
-// Search for Hashtag
+// Search for Hashtag Function
 // async because masto.v1.timelines.listHashtag needs await for promise
 async function getHashtag() {
 	const search = await masto.v1.timelines.listHashtag(hashtag);
 	return search;
 }
 
-
-// initialize json_data array
-let json_data = [],
+// initialize jsonData array
+let jsonData = [],
 		obj;
+
+function generateHashtag() {
+		// call function getHashtag and ad .then to get the result. need .then because getHashtag is async
+		getHashtag().then(search => {
+			for (let i = 0; i < search.length; i++) {
+				// Json read and write
+					//create tootID and id for json
+					let tootID = search[i].id;
+					// create a JSON object
+					obj = {};
+					obj['tootID'] = tootID;
+					obj['boosted'] = false;
+			
+					// add object to array
+					jsonData.push(obj);
+				}
+		}).catch(err => {
+			console.log(err);
+		});
+};
+
+// Boost Function
+function boost() {
+	for (let i = 0; i < jsonData.length; i++){
+		// check if toot is already boosted
+		// if not boosted (.boosted == false), boost it
+		if (jsonData[i].boosted == false) {
+			// boost toot id
+			masto.v1.statuses.reblog(jsonData[i].tootID);
+			// set state boosted to true
+			jsonData[i].boosted = true;
+			//jsonData.push(jsonData[i]);
+			console.log("Toot ID: " + jsonData[i].tootID + " boosted.");
+			console.log(" - - - - - - - - ");
+		} else {
+			console.log("Toot ID: " + jsonData[i].tootID + " already boosted.");
+			console.log(" - - - - - - - - ");
+		};
+	};
+};
+
+// Write JSON Function
+function writeJSON(){
+	// write json file
+	fs.writeFile(filepathenv, JSON.stringify(jsonData), {flags:'a'}, function(err){
+		if (err ) {
+			console.log('Error writing file', err)
+		} else
+		console.log('Successfully wrote file')
+	});
+};
 
 console.log("Search for Hashtag: #" + hashtag + " in the known Fediverse");
 console.log("Start in 60 seconds. Just wait.");
@@ -44,50 +92,14 @@ cron.schedule('* * * * *', () => {
 	console.log("Start searching for new toots.");
 	console.log(" - - - - - - - - ");
 
-	// call function getHashtag and ad .then to get the result. need .then because getHashtag is async
-	getHashtag().then(search => {
-		for (let i = 0; i < search.length; i++) {
-			// Json read and write
-				//create toot_id and id for json
-				let toot_id = search[i].id;
-				// create a JSON object
-				obj = {};
-				obj['toot_id'] = toot_id;
-				obj['boosted'] = false;
-		
-				// add object to array
-				json_data.push(obj);
-			}
-	}).catch(err => {
-		console.log(err);
-	});
+	// start search
+	generateHashtag();
 
-	// write JSON object to file
-	fs.writeFile(filepathenv, JSON.stringify(json_data), {flags:'a'}, function(err){
-		if (err ) {
-			console.log('Error writing file', err)
-		} else {
-			console.log('Successfully wrote file')
-			// for loop to boost all toots and check if they are already boosted
-			for (let i = 0; i < json_data.length; i++){
-				// check if toot is already boosted
-				if (json_data[i].boosted == true) {
-					// if not boosted, boost toot and set state to boosted
-				} else if (json_data[i].boosted == false) {
-					// boost toot id
-					masto.v1.statuses.reblog(json_data[i].toot_id);
-					// set state boosted to true
-					json_data[i].boosted = true;
-					console.log("Boost Toot ID: " + json_data[i].toot_id);
-					console.log("Toot ID: " + json_data[i].toot_id + " boosted.");
-					console.log(" - - - - - - - - ");
-				} else {
-					console.log("FAIL");
-				} {
-					
-				};
-			};
-			console.log("Search finished. Everything is boosted. Start again in 60 seconds. Happy tooting.")
-		}
-	});
+	// start boost
+	boost();
+
+	// write json
+	writeJSON();
+
+	console.log("Search finished. Everything is boosted. Start again in 60 seconds. Happy tooting.");
 });
