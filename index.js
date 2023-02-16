5dd855e6c5f787228a0e9a9b9e6ef702a61c99d1
gummipunkt <patti2012@gmail.com>
2023-02-16 23:59:29 +0100

    Update index.js
    complete rewritten. still work in progress.

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import { login } from 'masto';
 // ENV
 import dotenv from 'dotenv';
 // file system for json
-import fs from 'fs';
-// cron for scheduling
-import cron from 'node-cron';
+import fs, { write } from 'fs';
 
 // read .env file
 dotenv.config({ path: './.env' });
@@ -21,85 +19,119 @@ const masto = await login({
 // Import Hashtag
 const hashtag = process.env.HASHTAG;
 
-// Search for Hashtag Function
-// async because masto.v1.timelines.listHashtag needs await for promise
-async function getHashtag() {
-	const search = await masto.v1.timelines.listHashtag(hashtag);
-	return search;
-}
-
 // initialize jsonData array
 let jsonData = [],
 		obj;
 
-function generateHashtag() {
-		// call function getHashtag and ad .then to get the result. need .then because getHashtag is async
-		getHashtag().then(search => {
-			for (let i = 0; i < search.length; i++) {
-				// Json read and write
-					//create tootID and id for json
-					let tootID = search[i].id;
-					// create a JSON object
-					obj = {};
-					obj['tootID'] = tootID;
-					obj['boosted'] = false;
-			
-					// add object to array
-					jsonData.push(obj);
-				}
-		}).catch(err => {
-			console.log(err);
-		});
+function initialPush() {
+    obj = {
+        "tootID": "0",
+        "boosted": true
+    };
+    jsonData.push(obj);
+}
+
+initialPush();
+
+function startBoosting() {
+    // Search for Hashtag Function with a Promise
+    const search = new Promise((resolve, reject) => {
+        const promiseReceiveHashtag = masto.v1.timelines.listHashtag(hashtag);
+        if (promiseReceiveHashtag) {
+            resolve(promiseReceiveHashtag);
+            return promiseReceiveHashtag;
+        } else {
+            reject("Error");
+        }
+    });
+
+    // Start Promise Receiving Toots
+
+    search.then((promiseReceiveHashtag) => {
+        for (let i = 0; i < promiseReceiveHashtag.length; i++) {     
+            //create tootID and id for json
+            let tootID = promiseReceiveHashtag[i].id;
+            obj = {};
+            obj['tootID'] = tootID;
+            obj['boosted'] = false;
+            //console.log("jsonDataParse[1].tootID: " + jsonData[i].tootID);
+            //console.log("tootID: " + tootID);
+            if (jsonData.length > 1 && jsonData[i].tootID == 0) {
+                jsonData.splice(0, 1);
+                //console.log("Toot 0 removed.");
+            }
+
+            //FIXME: We need to find a way to check if the tootID is already in the array. Current state: everything is fine,
+            // but when a new toot is added, it will crashes because tootid and jsondata.tootid doesn't match anymore. :-/
+            //readtootsJS(); here perhaps?
+            
+            //if (jsonData.find(record => record.tootID === tootID) === true) {
+            if (jsonData[i].tootID == tootID) {
+            console.log("Toot already in array: " + tootID);
+            } else {
+            console.log("Toot not in array: " + tootID);
+            // add object to array
+            jsonData.push(obj);
+            }            
+        }
+    }).then((boost) => {
+        for (let i = 0; i < jsonData.length; i++) {
+            if (jsonData[i].boosted == false) {
+            masto.v1.statuses.reblog(jsonData[i].tootID);
+            jsonData[i].boosted = true;
+            console.log("Boosted: " + jsonData[i].tootID + " - New State: " + jsonData[i].boosted);
+            } else {
+                console.log("Already boosted: " + jsonData[i].tootID + " - Current State: " + jsonData[i].boosted);
+            }
+        }
+    })        
 };
 
-// Boost Function
-function boost() {
-	for (let i = 0; i < jsonData.length; i++){
-		// check if toot is already boosted
-		// if not boosted (.boosted == false), boost it
-		if (jsonData[i].boosted == false) {
-			// boost toot id
-			masto.v1.statuses.reblog(jsonData[i].tootID);
-			// set state boosted to true
-			jsonData[i].boosted = true;
-			//jsonData.push(jsonData[i]);
-			console.log("Toot ID: " + jsonData[i].tootID + " boosted.");
-			console.log(" - - - - - - - - ");
-		} else {
-			console.log("Toot ID: " + jsonData[i].tootID + " already boosted.");
-			console.log(" - - - - - - - - ");
-		};
-	};
+function writeTootsJS(){
+    const promiseWriteFile = new Promise((resolve, reject) => {
+        fs.writeFile(filepathenv, JSON.stringify(jsonData), {flags:'a'}, function(err, jsonData){
+            if (err ) {
+                console.log('Error writing file', err)
+            } else {
+                resolve(jsonData);
+                // clear jsonData array
+                console.log('Successfully wrote file')
+            }
+        });
+    }).catch(err => console.log(err)); 
+}
+
+function readTootsJS(){
+    const promiseReceiveFile = new Promise((resolve, reject) => {
+        fs.readFile(filepathenv, 'utf8', function(err, readTootFile){
+            if (err){
+                console.log(err);
+            } else {
+                resolve(readTootFile);
+            }
+        })
+    });
+    // Start Promise Receiving Toots
+    promiseReceiveFile.then((readTootFile) => {
+        console.log("File read success.");
+    })
 };
 
-// Write JSON Function
-function writeJSON(){
-	// write json file
-	fs.writeFile(filepathenv, JSON.stringify(jsonData), {flags:'a'}, function(err){
-		if (err ) {
-			console.log('Error writing file', err)
-		} else
-		console.log('Successfully wrote file')
-	});
-};
-
-console.log("Search for Hashtag: #" + hashtag + " in the known Fediverse");
-console.log("Start in 60 seconds. Just wait.");
-
-// start boosting every minute
-cron.schedule('* * * * *', () => {
-
+console.log("Start script in 20s.");
+function doStuff() {
 	console.log("Start searching for new toots.");
 	console.log(" - - - - - - - - ");
 
 	// start search
-	generateHashtag();
-
-	// start boost
-	boost();
-
-	// write json
-	writeJSON();
-
+	startBoosting();
+    writeTootsJS();
+    readTootsJS();
 	console.log("Search finished. Everything is boosted. Start again in 60 seconds. Happy tooting.");
-});
+  };
+
+  
+function run() {
+	setInterval(doStuff, 20000);
+};
+  
+run();
